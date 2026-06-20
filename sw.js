cf6949832ead9bea0702b096e7566651e32921e4
MarquinhoMax <marquinho88195999@gmail.com>
2026-06-20 03:36:05 -0300

    Atualiza service worker v3

--- sw.js
+++ sw.js
@@ -12,3 +12,36 @@ const FILES = [
     BASE + "script/script.js",
     BASE + "script/dados.js"
 ];
+
+// Instala e cria o cache
+self.addEventListener("install", event => {
+    self.skipWaiting();
+
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(FILES))
+    );
+});
+
+// Ativa imediatamente
+self.addEventListener("activate", event => {
+    event.waitUntil(
+        caches.keys().then(keys =>
+            Promise.all(
+                keys
+                    .filter(key => key !== CACHE_NAME)
+                    .map(key => caches.delete(key))
+            )
+        )
+    );
+
+    self.clients.claim();
+});
+
+// Busca primeiro no cache
+self.addEventListener("fetch", event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => response || fetch(event.request))
+    );
+});
